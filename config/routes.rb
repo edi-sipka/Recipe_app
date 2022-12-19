6ec8d9394afbc086f021353acfdd3e26d9152a3b
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-20 00:00:57 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,33 @@
 Rails.application.routes.draw do
-  get 'shopping_list/index'
-  get 'recipe_foods/index'
-  get 'recipes/index'
-  get 'inventory_foods/index'
-  get 'inventories/index'
-  get 'inventories/show'
-  get 'foods/index'
+  #get 'recipe_foods/index'
+  #get 'recipes/index'
+  #get 'inventory_foods/index'
+  #get 'inventories/index'
+  #get 'inventories/show'
+  #get 'foods/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  root to: "home#index"
+  resources :recipes do
+    resources :recipe_foods
+  end
+
+  resources :inventories do
+    resources :inventory_foods
+  end
+
+  root to: "foods#index"
+
+  resources :foods
+  resources :recipes do
+    resources :shopping_lists
+    resources :recipe_foods
+  end
+
+  resources :shopping_lists
+end
+
+  
   # Defines the root path route ("/")
   # root "articles#index"
-end
+
