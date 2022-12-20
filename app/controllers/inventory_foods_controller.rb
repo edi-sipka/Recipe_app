34e602a4bd7d9b05cd6f9ddc3c52d1c7bef22102
riyabulia12 <riyabulia12@gmail.com>
2022-12-21 00:50:01 +0530

    Add food list for each inventory

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -1,3 +1,26 @@
 class InventoryFoodsController < ApplicationController
-  def index; end
+  def index
+    @inventory_foods = InventoryFood.all
+  end
+
+  def new
+    @inventory_food = InventoryFood.new
+  end
+
+  def create
+    @inventory_food = InventoryFood.new(params[:inventory_food])
+    if @inventory_food.save
+      redirect_to @inventory_food, :notice => "Successfully created inventory food."
+    else
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    @inventory_food = InventoryFood.find(params[:id])
+    @food = Food.find(@inventory_food.food_id)
+    @food.destroy
+
+    redirect_back(fallback_location: root_path)
+  end
 end
