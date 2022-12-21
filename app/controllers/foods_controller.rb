c750f0ca781ebbfc0d5aa7d8efa27413e46d596c
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-21 12:58:36 +0100

    Add food controllers and functionality

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,26 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
+  def new 
+    @food = Food.new
+  end
+
+  def create 
+    @food = Food.new(food_params)
+
+    if @food.save 
+      flash[:success] = 'Food added successfully'
+      redirect_to foods_path
+    else
+      flash.now[:error] = 'Error: Food could not be added'
+      redirect_to new_food_path
+    end
+  end
+  
+  private 
+  
+  def food_params
+    params.require(:food).permit(:name, :price, :measurement_unit)
+  end
 end
