aa8cecc60f92d282c26151e69bbb6994ca7c7b4b
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-22 11:56:26 +0100

    Add model food testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,12 @@ Food.create!([
     {name: 'Cheese', measurement_unit: 'packet', price: 2}
 ])
 
-Inventory.create!([
-    {name: 'Fridge'},
-    {name: 'Freezer'},
-    {name: 'Pantry'}
-])
-
-InventoryFood.create!([
-    {food_id: 1, quantity: 3, inventory_id: 3},
-    {food_id: 2, quantity: 4, inventory_id: 1},
-    {food_id: 3, quantity: 4, inventory_id: 1},
-    {food_id: 4, quantity: 1, inventory_id: 2}
-])
-
+User.create!([
+    {user_id: 1, name: 'Edi', email: 'e@gmail.com', encrypted_password: '9min' },
+    ])
+    
 Recipe.create!([
     {user_id: 1, name: 'Chicken Salad', description: 'Chicken Salad', preparation_time: '9min', cooking_time: '3min', public: 1},
     {user_id: 1, name: 'Pineapple Chicken', description: 'Pineapple Chicken', preparation_time: '4min', cooking_time: '12min'},
-    {user_id: 1, name: 'Apple Pie', description: 'Apple Pie', preparation_time: '10min', cooking_time: '17min', public: 1},
-    {user_id: 2, name: 'Chicken Salad', description: 'Chicken Salad', preparation_time: '9min', cooking_time: '3min', public: 1},
-    {user_id: 2, name: 'Pineapple Chicken', description: 'Pineapple Chicken', preparation_time: '4min', cooking_time: '22min'},
-    {user_id: 2, name: 'Apple Pie', description: 'Apple Pie', preparation_time: '10min', cooking_time: '17min', public: 1}
+    {user_id: 1, name: 'Apple Pie', description: 'Apple Pie', preparation_time: '10min', cooking_time: '17min', public: 1}
 ])
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ subject do
+  @user = User.create!(name:"Edi", role:"user", email:"e2e@gmail.com", password:"ediedi" )
+  Food.create(name:"banana", measurement_unit:"grams", price:2, user:@user)
+ end
+ before(:each) {subject.save}
+
+ it "should have name banana" do
+  expect(subject.name).to eql("banana")
+ end
+
+ it "should have price of 2" do
+  expect(subject.price).to eql(2)
+ end
+
+ it "Price should be 0 or higher" do
+  subject.price = -5
+  expect(subject).to_not be_valid
+ end
 end
