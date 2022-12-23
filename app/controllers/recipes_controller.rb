9fada774d275cee0644c474c59d2dfcdbd2e99be
riyabulia12 <riyabulia12@gmail.com>
2022-12-23 23:22:50 +0530

    Fixed n+1 problems

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,7 +22,7 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
-    @recipe_foods = @recipe.recipe_foods
+    @recipe_foods = @recipe.recipe_foods.includes(:food)
     @inventories = Inventory.all
   end
 
